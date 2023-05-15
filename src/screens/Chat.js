ce5f0b1e299104935c477b9ba6cba25680562310
Joseph Tey <jjttey@gmail.com>
2023-05-14 23:00:49 -0700

    new stuff for code in place

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -10,6 +10,9 @@ import ContentEditable from "react-contenteditable";
 import { Typewriter } from "typewriting-react";
 import LizStationary from "../img/liz_stationary.gif";
 import Liz from "../img/liz.gif";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-monokai";
 import textToSpeech from "../utils/elevenLabs";
 import {
   generateEssaySystemPrompt,
@@ -43,7 +46,6 @@ const Chat = () => {
   });
 
   const [promptText, setPromptText] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [AIState, setAIState] = useState("thinking");
   // thinking -> gpt generation...
   // speaking -> liz is talking (typewriter animation!)
@@ -82,8 +84,8 @@ const Chat = () => {
     };
     if (textInput.current) {
       if (!isLoading) {
-        setAIState("speaking");
-        setTextToVoice(messages[messages.length - 1].content);
+        setAIState("waiting");
+        // setTextToVoice(messages[messages.length - 1].content);
         update();
         // generate voices
         // show the animation of streaming ...
@@ -149,153 +151,139 @@ const Chat = () => {
 
   useEffect(() => {
     if (AIState === "speaking") {
-      textToSpeech("21m00Tcm4TlvDq8ikWAM", textToVoice, elevenLabsAPI, {
-        stability: 0.75,
-        similarity_boost: 0.6,
-      });
+      // textToSpeech("21m00Tcm4TlvDq8ikWAM", textToVoice, elevenLabsAPI, {
+      //   stability: 0.6,
+      //   similarity_boost: 0.6,
+      // });
     }
   }, [AIState]);
 
   return data ? (
-    <>
-      <div className="flex w-1/2 mx-96 my-48 justify-left flex-wrap flex-col relative">
-        <span
-          className={`w-48 text-center text-gray-600 fade-in mb-4 font-serif text-xl`}
-        >
-          {/* <AudioStream
-            voiceId={"21m00Tcm4TlvDq8ikWAM"}
-            text={textToVoice}
-            apiKey={elevenLabsAPI}
-            voiceSettings={{
-              stability: 0.75,
-              similarity_boost: 0.6,
-            }}
-            className="text-gray-100"
-            triggerVariable={{ isLoading }}
-          /> */}
-          {AIState === "thinking" ? (
-            <>
-              <b>Liz</b> is thinking...
-            </>
-          ) : (
-            <b>Liz</b>
-          )}
-        </span>
-        {AIState === "thinking" ? (
-          <img src={Liz} className="rounded-lg w-48 mb-16" />
-        ) : (
-          <img src={LizStationary} className="rounded-lg w-48 mb-16" />
-        )}
-        <div>
-          {messages.length < 1
-            ? null
-            : !isLoading &&
-              messages.map((msg, i) => {
-                return i > messages.length - 2 ? (
-                  msg.role === "assistant" ? (
-                    <div className="font-serif text-3xl text-left" id="content">
-                      <Typewriter
-                        words={[msg.content]}
-                        onWordFinishTyping={() => {
-                          setAIState("waiting");
-                        }}
-                      />
-                    </div>
-                  ) : (
-                    <div className="font-serif text-2xl text-right mb-16 mt-4 text-purple-600">
-                      {msg.content.split("<br />").map((para) => {
-                        return <p>{para}</p>;
-                      })}
-                    </div>
-                  )
-                ) : null;
-              })}
-        </div>
-        {AIState === "listening" ? (
-          <>
-            <span className="text-purple-600 mt-2 opacity-70 flex flex-row gap-2 items-center fade-in">
-              <div class="dot dot--basic"></div>{" "}
-              <div>
-                Liz is now <b>listening...</b> start speaking! Press 'enter' to
-                finish.
-              </div>
-            </span>
-          </>
-        ) : AIState === "waiting" ? (
-          <>
-            <span className="text-gray-600 mt-2 opacity-70 flex flex-row gap-2 items-center fade-in">
-              <div>
-                Take a few seconds to understand the question. Press{" "}
-                <b>enter</b> when you're ready to start speaking.
-              </div>
-            </span>
-          </>
-        ) : AIState === "editing" ? (
-          <>
-            <span className="text-gray-600 mt-2 opacity-70 flex flex-row gap-2 items-center fade-in">
-              <div>
-                If we transcribed your answer inaccurately, feel free to{" "}
-                <b>edit</b> it before clicking send.
-              </div>
-            </span>
-          </>
-        ) : null}
-        <div className="flex flex-col gap-4 items-end">
-          <ContentEditable
-            disabled={AIState !== "editing" || isLoading} // use true to disable editing
-            onChange={(e) => {
-              setPromptText(e.target.value);
-            }}
-            ref={textInput}
-            class="border-0 outline-none font-serif text-2xl text-purple-600 w-full bg-white mt-16 text-right"
-            html={promptText ? promptText : null}
-          />
-
-          {AIState === "editing" && !transcribing && !recording && !speaking ? (
-            <Button
-              type="dashed"
-              className="align-right"
-              onClick={() => {
-                onSend();
+    <div className="flex flex-col gap-4 items-center pt-48 w-full px-48">
+      <div className="flex flex-row gap-4 justify-center w-full">
+        <div className="flex w-1/2 items-center pt-8 flex-wrap flex-col relative bg-white rounded-lg drop-shadow-md">
+          <span
+            className={`w-48 text-center text-gray-600 fade-in mb-4 font-serif text-md`}
+          >
+            {/* <AudioStream
+              voiceId={"21m00Tcm4TlvDq8ikWAM"}
+              text={textToVoice}
+              apiKey={elevenLabsAPI}
+              voiceSettings={{
+                stability: 0.75,
+                similarity_boost: 0.6,
               }}
-            >
-              Send
-            </Button>
-          ) : null}
-        </div>
-        <Modal
-          title="Reference"
-          open={isModalOpen}
-          onOk={() => {
-            setIsModalOpen(false);
-          }}
-          onCancel={() => {
-            setIsModalOpen(false);
-          }}
-        >
-          {data?.type === "code" ? (
-            <div>
-              {data.code.split("\n").map((para) => {
-                return <p>{para}</p>;
-              })}
-            </div>
-          ) : (
-            <div>{data.essay}</div>
-          )}
-        </Modal>
-        <Button
-          className="absolute top-4 right-4"
-          onClick={() => {
-            setIsModalOpen(true);
-          }}
-        >
-          See{" "}
-          <span className="ml-1">
-            {data?.type === "essay" ? <>Essay</> : <>Code</>}
+              className="text-gray-100"
+              triggerVariable={{ isLoading }}
+            /> */}
+            {AIState}
           </span>
-        </Button>
+          {AIState === "thinking" ? (
+            <img src={Liz} className="rounded-lg w-24 mb-8" />
+          ) : (
+            <img src={LizStationary} className="rounded-lg w-24 mb-8" />
+          )}
+          <div>
+            {messages.length < 1
+              ? null
+              : messages.map((msg, i) => {
+                  return i > messages.length - 2 ? (
+                    msg.role === "assistant" ? (
+                      <div
+                        className="font-serif text-lg text-left px-8"
+                        id="content"
+                      >
+                        {msg.content}
+                      </div>
+                    ) : (
+                      <div className="font-serif text-lg text-right mb-16 mt-4 text-purple-600">
+                        {msg.content.split("<br />").map((para) => {
+                          return <p>{para}</p>;
+                        })}
+                      </div>
+                    )
+                  ) : null;
+                })}
+          </div>
+          {AIState === "listening" ? (
+            <>
+              <span className="text-purple-600 mt-2 opacity-70 flex flex-row gap-2 items-center fade-in">
+                <div class="dot dot--basic"></div>{" "}
+                <div>
+                  Liz is now <b>listening...</b> start speaking! Press 'enter'
+                  to finish.
+                </div>
+              </span>
+            </>
+          ) : AIState === "waiting" ? (
+            <>
+              <span className="text-gray-600 mt-2 opacity-70 flex flex-row gap-2 items-center fade-in">
+                <div>
+                  Take a few seconds to understand the question. Press{" "}
+                  <b>enter</b> when you're ready to start speaking.
+                </div>
+              </span>
+            </>
+          ) : AIState === "editing" ? (
+            <>
+              <span className="text-gray-600 mt-2 opacity-70 flex flex-row gap-2 items-center fade-in">
+                <div>
+                  If we transcribed your answer inaccurately, feel free to{" "}
+                  <b>edit</b> it before clicking send.
+                </div>
+              </span>
+            </>
+          ) : null}
+          <div className="flex flex-col gap-4 items-end">
+            <ContentEditable
+              disabled={AIState !== "editing" || isLoading} // use true to disable editing
+              onChange={(e) => {
+                setPromptText(e.target.value);
+              }}
+              ref={textInput}
+              class="border-0 outline-none font-serif text-2xl text-purple-600 w-full bg-white mt-16 text-right"
+              html={promptText ? promptText : null}
+            />
+
+            {AIState === "editing" &&
+            !transcribing &&
+            !recording &&
+            !speaking ? (
+              <Button
+                type="dashed"
+                className="align-right"
+                onClick={() => {
+                  onSend();
+                }}
+              >
+                Send
+              </Button>
+            ) : null}
+          </div>
+        </div>
+        <AceEditor
+          id="editor"
+          mode="python"
+          theme="monokai"
+          onChange={() => {}}
+          highlightActiveLine={false}
+          showGutter={true}
+          wrapEnabled={true}
+          maxLines={null}
+          name="editor"
+          value={data.code}
+          editorProps={{ $blockScrolling: true }}
+          style={{
+            width: "50%",
+          }}
+          className="rounded-lg w-1/2"
+        />
       </div>
-    </>
+      <div className="flex bg-white rounded-lg drop-shadow-md h-32 p-8 w-full flex-row gap-4">
+        <input className="bg-gray-50 rounded-lg p-8 w-full outline-none text-gray-700"></input>
+        <Button>Send!</Button>
+      </div>
+    </div>
   ) : (
     <div className="flex w-full justify-center content-center h-screen flex-wrap flex-col">
       <ReactLoading
